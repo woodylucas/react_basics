38beaea189a8794d3ac53400cab8aa3c69eef847
woodylucas <wlucas0311@gmail.com>
2020-10-24 23:19:28 -0400

    functional components

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,24 @@
-class Hello extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello there!</h1> 
-                <h1>Hello there!</h1> 
-                <h1>Hello there!</h1> 
-            </div>
-        )
-    }
+// class Hello extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello there!</h1> 
+//                 <h1>Hello there!</h1> 
+//                 <h1>Hello there!</h1> 
+//             </div>
+//         )
+//     }
+// }
+
+function Hello() {
+    return (
+        <div>
+            <h1>Hello There!</h1>
+            <h1>Hello There!</h1>
+            <h1>Hello There!</h1>
+            <h1>Hello There!</h1>
+        </div>
+    );
 }
 
 ReactDOM.render(<Hello/>, document.getElementById('root'));
